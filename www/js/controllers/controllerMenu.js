71af50a5cfe065ed92cadae57615257df6600aba
Dyego Rodrigo <dyego9444@hotmail.com>
2016-03-13 20:01:57 -0300

    options menu and whatsapp functionality

---
+++ www/js/controllers/controllerMenu.js
@@ -0,0 +1,27 @@
+var app = angular.module('mobile');
+
+app.controller("MenuCtrl", MenuCtrl);
+
+function MenuCtrl($scope, $ionicPopover){
+    $scope.openWhatsApp = function(){
+        cordova.plugins.Whatsapp.send("11985694648");
+    };
+
+    $ionicPopover.fromTemplateUrl('templates/popover/config.html', {
+        scope: $scope
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
+
+
+    $scope.openPopover = function($event) {
+        $scope.popover.show($event);
+    };
+    $scope.closePopover = function() {
+        $scope.popover.hide();
+    };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
+}
